0343e5ed111acfce79608b814b48c425dc3f21c4
miodrag <mpavkovic@gmail.com>
2016-07-25 11:18:38 +0200

    agenda sql script updated

--- scripts/smiletime_agenda.sql
+++ scripts/smiletime_agenda.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE  IF NOT EXISTS `smiletime` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `smiletime`;
+DROP TABLE IF EXISTS `agenda`;
 CREATE TABLE `agenda` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(200) NOT NULL,
